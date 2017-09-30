d6804d32552ea5cb46bfac3f2a74419dc10641e2
HGalitska <h.galitskaya@gmail.com>
2017-09-30 19:51:19 +0300

    Added files to review

---
+++ 7 Serialization/src/Main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[]args){
+        Client client = new Client();
+        client.startConnection("127.0.0.1", 6666);
+        Cat bob = new Cat("Bob", 3, "good");
+        Cat response = client.sendMessage(bob);
+        response.sayMeow();
+    }
+}
