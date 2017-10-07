094edb06c3550d1663f6ef89a450ee69dd396d3e
HGalitska <h.galitskaya@gmail.com>
2017-10-07 23:58:14 +0300

    Revert "Removed files from master."
    This reverts commit f77b709ab6f6160759ed8d86216e274b01fdd4af.

---
+++ 7 Serialization/src/Server.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private ServerSocket serverSocket = null;
+    private Socket clientSocket = null;
+    private ObjectOutputStream output = null;
+    private ObjectInputStream input = null;
+
+    public void start(int port) {
+        Cat message = null;
+        try {
+            serverSocket = new ServerSocket(port);
+            clientSocket = serverSocket.accept();
+            output = new ObjectOutputStream(clientSocket.getOutputStream());
+            input = new ObjectInputStream(clientSocket.getInputStream());
+            message = (Cat)input.readObject();
+            output.writeObject(message);
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stop() {
+        try {
+            input.close();
+            output.close();
+            clientSocket.close();
+            serverSocket.close();
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.start(6666);
+    }
+}
